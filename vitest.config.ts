e02f423baf84b788bfc4c6bbaf0b3c1beb9189b8
imeankenshin <omurakenshin@gmail.com>
2025-04-10 16:41:59 +0900

    不要なインポートを削除

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,5 @@
 // vitest.config.ts
 import { defineVitestConfig } from "@nuxt/test-utils/config";
-import { fileURLToPath } from 'node:url';
-import { dirname } from 'node:path';
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineVitestConfig({
   test: {
