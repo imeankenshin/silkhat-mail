583ecf28021402753309a28c001d9d1e08ec0ac4
imeankenshin <omurakenshin@gmail.com>
2025-07-03 16:39:00 +0900

    Delete unneceesary class

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <UiSidebarProvider class="w-full max-w-screen flex relative">
     <AppSidebar />
-    <main class="flex-col w-full min-w-0">
+    <main class="w-full min-w-0">
       <div class="z-10 p-1 sticky inset-0 flex">
         <div class="p-2 bg-background/30 backdrop-blur rounded-lg">
           <UiSidebarTrigger />
