c1522b3f92191f924cc96293b33752bc40bb74e4
imeankenshin <omurakenshin@gmail.com>
2025-09-02 12:37:18 +0900

    use material symbols instead of lucid

--- app/components/ui/dialog/DialogContent.vue
+++ app/components/ui/dialog/DialogContent.vue
@@ -37,7 +37,7 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits)
       <DialogClose
         class="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4"
       >
-        <Icon name="i-lucide-x" />
+        <Icon name="material-symbols:close-rounded" />
         <span class="sr-only">Close</span>
       </DialogClose>
     </DialogContent>
